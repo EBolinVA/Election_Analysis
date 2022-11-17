65b3418b7ccb8d38f10408a35fad131ee1c8071c
EBolinVA <elizabeth.bolin@gmail.com>
2022-11-16 22:45:16 -0500

    upload PyPoll.py and create analysis folder

---
+++ PyPoll.py
@@ -0,0 +1,33 @@
+# The data we need to retrieve
+import csv
+import os
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # To do: perform analysis
+    print(election_data)
+
+#Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Use the open statement to open the file as a text file.
+outfile = open(file_to_save, "w")
+
+#Write some data to the file
+outfile.write("Hello World")
+
+#1. The total number of votes cast
+
+#2. A complete list of candidates who received votes
+
+#3. The percentage of votes each candidate won.
+
+#4. The total number of votes each candidate won. 
+
+#5. The winner of the election based on popular vote.
+
+
+
