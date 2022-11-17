9d5f64633cb5d80a3b889b1540786e4c830efa3a
EBolinVA <elizabeth.bolin@gmail.com>
2022-11-17 06:45:56 -0500

    update Pypoll.py and add .csv to Resources

--- PyPoll.py
+++ PyPoll.py
@@ -4,24 +4,22 @@ import os
 # Assign a variable for the file to load and the path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 
+#Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
-    # To do: perform analysis
-    print(election_data)
 
-#Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+    # To do: read and analyze the data here.
+    file_reader = csv.reader(election_data)
 
-# Using the with statement open the file as a text file.
-with open(file_to_save, "w") as txt_file:
+    # Read and Print the header row in the CSV file.
+    headers = next(file_reader)
+    print(headers)
 
-    #Write a header and a separater
-    txt_file.write("Counties in the Election\n")
-    txt_file.write("--------------------------\n")
 
-    #Write three counties to the file.
-    txt_file.write("Arapahoe\nDenver\nJefferson")
+
 
 #1. The total number of votes cast
 
